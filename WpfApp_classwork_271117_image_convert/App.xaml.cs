e0ab283961f1971f8416742204d29ea34b0f0829
Alexandr Momotuik <sasha.momotuik@gmail.com>
2017-11-27 19:46:11 +0200

    Add project files.

---
+++ WpfApp_classwork_271117_image_convert/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp_classwork_271117_image_convert
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
